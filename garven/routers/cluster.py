4f1288e756db8642a2ab163e6ebe1a5118d08bd1
skelmis <ethan@koldfusion.xyz>
2022-12-11 00:01:13 +1300

    fix: handle missing / failing clusters

--- garven/routers/cluster.py
+++ garven/routers/cluster.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 import logging
 import os
+from copy import deepcopy
 from typing import TYPE_CHECKING
 
 from fastapi import APIRouter, Depends
 from fastapi import Request
+from zonis import RequestFailed
 
 from garven.dependencies import get_auth_header
 from garven.schema.cluster import ClusterHealth
@@ -24,12 +26,17 @@ cluster_router = APIRouter(
 
 @cluster_router.get("/status", response_model=ClusterHealth)
 async def cluster_status(request: Request):
+    partial_response = False
     z: Server = request.app.zonis
     d = await z.request_all("cluster_status")
+    for k, item in deepcopy(d.items()):
+        if isinstance(item, RequestFailed):
+            partial_response = True
+            log.error("/cluster/status WS threw '%s'", item.response_data)
+            d.pop(k)
+            continue
 
     if len(d.values()) != int(os.environ.get("CLUSTER_COUNT", 6)):
         partial_response = True
-    else:
-        partial_response = False
 
     return ClusterHealth(clusters=d, partial_response=partial_response)
